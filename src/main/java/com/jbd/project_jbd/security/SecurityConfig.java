4902cb1ec3ae2b7a96824eecd6a11840d767cc51
Vadim <vadimka20034@gmail.com>
2025-12-07 16:00:59 +0300

    Добавлен сваггер

--- src/main/java/com/jbd/project_jbd/security/SecurityConfig.java
+++ src/main/java/com/jbd/project_jbd/security/SecurityConfig.java
@@ -40,9 +40,31 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        // Доступные всем.
+                        .requestMatchers("/").permitAll()
+                        // Swagger.
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        // Авторизация и регистрация.
+                        .requestMatchers("/api/auth/**"   ).permitAll()
+                        .requestMatchers("/api/registration"   ).permitAll()
+                        // Администратор.
+                        .requestMatchers("/api/admin/").hasRole("ADMIN")
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        // Группы задач.
+                        .requestMatchers("/api/groups").authenticated()
+                        .requestMatchers("/api/groups/**").authenticated()
+                        // Задачи.
+                        .requestMatchers("/api/tasks").authenticated()
+                        .requestMatchers("/api/tasks/**").authenticated()
+                        // Статусы задач.
+                        .requestMatchers("/api/task-status").authenticated()
+                        .requestMatchers("/api/task-status/**").authenticated()
+                        // Пользователи.
+                        .requestMatchers("/api/users/me").authenticated()
+                        // Api.
                         .requestMatchers("/api/**").authenticated()
+                        // Остальное.
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
